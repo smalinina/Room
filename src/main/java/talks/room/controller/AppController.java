8e610e48cd96d41eedd4b872f88cdb13437105a8
smalinina <malinina2395@gmail.com>
2018-09-05 18:06:43 +0500

    add spring jdbc

--- src/main/java/talks/room/controller/AppController.java
+++ src/main/java/talks/room/controller/AppController.java
@@ -1,14 +1,30 @@
 package talks.room.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import talks.room.model.CreateTable;
 
 @Controller
 public class AppController {
 
+
+    private final CreateTable createTable;
+
+    @Autowired
+    public AppController(CreateTable createTable) {
+        this.createTable = createTable;
+    }
+
     @RequestMapping("/")
     public String getHelloPage(Model model) {
         return "hello";
+    }
+
+    @RequestMapping("/create")
+    public String createTableStatus(Model model) {
+        model.addAttribute("status", createTable.create());
+        return "create";
     }
 }
